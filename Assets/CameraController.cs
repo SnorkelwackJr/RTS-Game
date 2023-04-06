ad48939233c1dfb11d8e16b26e8fba8c90e802c7
Jacob Nicholls <jacob8287@gmail.com>
2023-04-06 01:18:31 -0700

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    public float movementSpeed;
+    public float movementTime;
+    public Vector3 zoomAmount;
+
+    public Vector3 newPosition;
+    public Vector3 newZoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+    }
+
+    void HandleMovementInput()
+    {
+        // WASD movement
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed * (newZoom.y / 8));
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed * (newZoom.y / 8));
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed * (newZoom.y / 8));
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -movementSpeed * (newZoom.y / 8));
+        }
+
+        // Zoom in/out
+        Vector3 rigOrigin = new Vector3(0, 1, 0);
+        if (Input.GetKey(KeyCode.Q))
+        {
+            // zoom in boundary
+            if (newZoom.y > 5 && newZoom.z < 5) 
+            {
+                newZoom += zoomAmount;
+            }
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            // zoom out boundary
+            if (newZoom.y < rigOrigin.y+80 && newZoom.z < rigOrigin.z+80) 
+            {
+                newZoom -= zoomAmount;
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+    }
+}
