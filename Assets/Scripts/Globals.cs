4d050e9e1c1a7a85fdc14249e834aaf9e187ff52
Killef <mr.killef@gmail.com>
2023-04-08 13:31:39 -0600

    auto instantiating buttons

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,30 +1,14 @@
-using System.Collections.Generic;
-
 public class Globals
 {
     public static int TERRAIN_LAYER_MASK = 1 << 8;
-    public static List<UnitManager> SELECTED_UNITS = new List<UnitManager>();
 
     public static BuildingData[] BUILDING_DATA = new BuildingData[]
     {
-        new BuildingData("House", 100, new Dictionary<string, int>()
-        {
-            { "gold", 100 },
-            { "wood", 120 }
-        }),
-        new BuildingData("Tower", 100, new Dictionary<string, int>()
-        {
-            { "gold", 80 },
-            { "wood", 80 },
-            { "stone", 100 }
-        })
+        new BuildingData("Silo", 100)
     };
 
-    public static Dictionary<string, GameResource> GAME_RESOURCES =
-        new Dictionary<string, GameResource>()
-        {
-            { "gold", new GameResource("Gold", 0) },
-            { "wood", new GameResource("Wood", 0) },
-            { "stone", new GameResource("Stone", 0) }
-        };
+    /*public static UnitData[] UNIT_DATA = new UnitData[]
+    {
+        new UnitData("Unit", 100)
+    }*/
 }
--- Assets/Scripts/UIScripts/UIManager.cs
+++ Assets/Scripts/UIScripts/UIManager.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Transform buildingMenu;
+    private BlueprintScript _blueprintScript;
+
+    public Transform contextualMenu;
     public GameObject buildingButtonPrefab;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-        
+        _blueprintScript = GetComponent<BlueprintScript>();
+
+        for (int i = 0; i < Globals.BUILDING_DATA.Length; i++)
+        {
+            GameObject button = GameObject.Instantiate(
+                buildingButtonPrefab,
+                contextualMenu);
+            string code = Globals.BUILDING_DATA[i].Code;
+            button.name = code;
+            button.transform.Find("Text").GetComponent<Text>().text = code;
+            Button b = button.GetComponent<Button>();
+            //_AddBuildingButtonListener(b, i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /*private void _AddBuildingButtonListener(Button b, int i)
     {
-        
-    }
+        b.onClick.AddListener(() => _blueprintScript.SelectPlacedBuilding(i));   
+    }*/
 }
