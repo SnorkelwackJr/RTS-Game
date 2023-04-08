0d216dcbb880f1a26cb5879ed4f0d0b776b86094
Jacob Nicholls <jacob8287@gmail.com>
2023-04-07 21:42:07 -0700

    Redo building placement...again

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -2,9 +2,9 @@ public class Globals
 {
     public static int TERRAIN_LAYER_MASK = 1 << 8;
 
-    /*public static BuildingData[] BUILDING_DATA = new BuildingData[]
+    public static BuildingData[] BUILDING_DATA = new BuildingData[]
     {
         new BuildingData("Building", 100)
-    };*/
+    };
 
 }
