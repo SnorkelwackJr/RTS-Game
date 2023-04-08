7c690c7e02c1946a9ed9b083b6b4957e454e7845
Killef <mr.killef@gmail.com>
2023-04-08 13:31:39 -0600

    auto instantiating buttons

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -2,9 +2,13 @@ public class Globals
 {
     public static int TERRAIN_LAYER_MASK = 1 << 8;
 
-    /*public static BuildingData[] BUILDING_DATA = new BuildingData[]
+    public static BuildingData[] BUILDING_DATA = new BuildingData[]
     {
-        new BuildingData("Building", 100)
-    };*/
+        new BuildingData("Silo", 100)
+    };
 
+    /*public static UnitData[] UNIT_DATA = new UnitData[]
+    {
+        new UnitData("Unit", 100)
+    }*/
 }
