f94285b99c865c0a3f7ba92f1fc8df31e316c68e
Jacob Nicholls <jacob8287@gmail.com>
2023-04-17 23:10:44 -0700

    First half of Tutorial 12

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,8 +2,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Ray _ray;
+    private RaycastHit _raycastHit;
     private void Awake()
     {
         DataHandler.LoadGameData();
+    }
+
+    private void Update()
+    {
+        _CheckUnitsNavigation();
+    }
+
+    private void _CheckUnitsNavigation()
+    {
+        if (Globals.SELECTED_UNITS.Count > 0 && Input.GetMouseButtonUp(1))
+        {
+            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(
+                _ray,
+                out _raycastHit,
+                1000f,
+                Globals.TERRAIN_LAYER_MASK
+            ))
+            {
+                foreach (UnitManager um in Globals.SELECTED_UNITS)
+                    if (um.GetType() == typeof(CharacterManager))
+                        ((CharacterManager)um).MoveTo(_raycastHit.point);
+            }
+        }
     }
 }
--- Assets/Scripts/Units/Characters/CharacterManager.cs
+++ Assets/Scripts/Units/Characters/CharacterManager.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharacterManager : UnitManager
 {
+    public NavMeshAgent agent;
+
     private Character _character;
     public override Unit Unit
     {
         get { return _character; }
         set { _character = value is Character ? (Character)value : null; }
+    }
+
+    public void MoveTo(Vector3 targetPosition)
+    {
+        agent.destination = targetPosition;
     }
 }
